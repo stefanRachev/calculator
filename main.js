ddba32734508b79f6f77ef4fa85b52e3702738d1
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2024-03-13 17:01:55 +0200

    tets functionality

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ for (let key of keys) {
       input = input.slice(0, -1);
       display_input.innerHTML = cleanInput(input);
     } else if (value == "=") {
-      let result = eval(input);
-      display_output.innerHTML = result;
+      let result = eval(prepareInput(input));
+      display_output.innerHTML = cleanOutput(result);
     } else if (value == "brackets") {
       if (
         input.indexOf("(") == -1 ||
@@ -36,8 +36,10 @@ for (let key of keys) {
       }
       display_input.innerHTML = cleanInput(input);
     } else {
-      input += value;
-      display_input.innerHTML = cleanInput(input);
+      if (validateInput(value)) {
+        input += value;
+        display_input.innerHTML = cleanInput(input);
+      }
     }
   });
 }
@@ -59,10 +61,60 @@ function cleanInput(input) {
       inputArray[i] = `<span class="brackets">(</span>`;
     } else if (inputArray[i] == ")") {
       inputArray[i] = `<span class="brackets">)</span>`;
-    }else if (inputArray[i] == "%") {
-        inputArray[i] = ` <span class=percent">%</span> `;
-      }
+    } else if (inputArray[i] == "%") {
+      inputArray[i] = ` <span class=percent">%</span> `;
+    }
   }
 
+  return inputArray.join("");
+}
+
+function cleanOutput(output) {
+  let outputString = output.toString();
+  let decimal = outputString.split(".")[1];
+  outputString = outputString.split(".")[0];
+
+  let outputArray = outputString.split("");
+
+  if (outputArray.length > 3) {
+    for (let i = outputArray.length - 3; i > 0; i -= 3) {
+      outputArray.splice(i, 0, ",");
+    }
+  }
+
+  if (decimal) {
+    outputArray.push(".");
+    outputArray.push(".");
+  }
+
+  return outputArray.join("");
+}
+
+function validateInput(value) {
+  let lastInput = input.slice(-1);
+  let operators = ["+", "-", "*", "/"];
+
+  if (value == "." && lastInput == ".") {
+    return false;
+  }
+
+  if (operators.includes(value)) {
+    if (operators.includes(lastInput)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  return true;
+}
+
+function prepareInput(input) {
+  let inputArray = input.split("");
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] == "%") {
+      inputArray[i] = "/100";
+    }
+  }
   return inputArray.join("");
 }
