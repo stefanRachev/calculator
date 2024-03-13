3c9fbc8ddac008cd0bc7efb8f4107f3f2c27b906
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2024-03-13 15:49:09 +0200

    create  function cleanInput

--- main.js
+++ main.js
@@ -14,34 +14,55 @@ for (let key of keys) {
       display_output.innerHTML = "";
     } else if (value == "backspace") {
       input = input.slice(0, -1);
-      display_input.innerHTML = input;
+      display_input.innerHTML = cleanInput(input);
     } else if (value == "=") {
       let result = eval(input);
       display_output.innerHTML = result;
     } else if (value == "brackets") {
       if (
         input.indexOf("(") == -1 ||
-        input.indexOf("(") != -1 &&
-        input.indexOf(")") != -1 &&
-        input.lastIndexOf("(") < input.lastIndexOf(")")
+        (input.indexOf("(") != -1 &&
+          input.indexOf(")") != -1 &&
+          input.lastIndexOf("(") < input.lastIndexOf(")"))
       ) {
         input += "(";
       } else if (
-        input.indexOf("(") != -1 &&
-        input.indexOf(")") == -1 ||
-        input.indexOf("(") != -1 &&
-        input.indexOf(")") != -1 &&
-        input.lastIndexOf("(") > input.lastIndexOf(")")
+        (input.indexOf("(") != -1 && input.indexOf(")") == -1) ||
+        (input.indexOf("(") != -1 &&
+          input.indexOf(")") != -1 &&
+          input.lastIndexOf("(") > input.lastIndexOf(")"))
       ) {
         input += ")";
       }
-      display_input.innerHTML = input;
-    }else{
-        input += value;
-        display_input.innerHTML = input;
+      display_input.innerHTML = cleanInput(input);
+    } else {
+      input += value;
+      display_input.innerHTML = cleanInput(input);
     }
   });
 }
 
+function cleanInput(input) {
+  let inputArray = input.split("");
+  let inputArrayLength = inputArray.length;
 
+  for (let i = 0; i < inputArrayLength; i++) {
+    if (inputArray[i] == "*") {
+      inputArray[i] = ` <span class="operator">x</span> `;
+    } else if (inputArray[i] == "/") {
+      inputArray[i] = ` <span class="operator">/</span> `;
+    } else if (inputArray[i] == "+") {
+      inputArray[i] = ` <span class="operator">+</span> `;
+    } else if (inputArray[i] == "-") {
+      inputArray[i] = ` <span class="operator">-</span> `;
+    } else if (inputArray[i] == "(") {
+      inputArray[i] = `<span class="brackets">(</span>`;
+    } else if (inputArray[i] == ")") {
+      inputArray[i] = `<span class="brackets">)</span>`;
+    }else if (inputArray[i] == "%") {
+        inputArray[i] = ` <span class=percent">%</span> `;
+      }
+  }
 
+  return inputArray.join("");
+}
